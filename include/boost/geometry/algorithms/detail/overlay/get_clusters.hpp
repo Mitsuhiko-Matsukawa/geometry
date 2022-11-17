bbb6f6d1f1fd18b0db639b332b0a1ef2b5240502
matsu <matsukawa@zodiacz.com>
2022-11-17 12:45:03 +0800

    [overlay] fix cluster by adapting tolerance https://github.com/boostorg/geometry/pull/1093

--- include/boost/geometry/algorithms/detail/overlay/get_clusters.hpp
+++ include/boost/geometry/algorithms/detail/overlay/get_clusters.hpp
@@ -35,20 +35,28 @@ namespace detail { namespace overlay
 template <typename Tag = no_rescale_policy_tag, bool Integral = false>
 struct sweep_equal_policy
 {
+private:
+    template <typename T>
+    static inline T threshold()
+    {
+        // Tuned by cases of #1081
+        return T(3);
+    }
+public:
     template <typename P>
     static inline bool equals(P const& p1, P const& p2)
     {
-        // Points within a epsilon are considered as equal
+        // Points within a some epsilons are considered as equal
         using coor_t = typename coordinate_type<P>::type;
-        return approximately_equals(p1, p2, coor_t(1));
+        return approximately_equals(p1, p2, threshold<coor_t>());
     }
 
     template <typename T>
     static inline bool exceeds(T value)
     {
         // This threshold is an arbitrary value
-        // as long as it is than the used epsilon
-        T const limit = T(1) / T(1);
+        // as long as it is bigger than the used value above
+        T const limit = T(1) / threshold<T>();
         return value > limit;
     }
 };
